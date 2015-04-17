d81797898d677bff1ef8307f12a383684f3896d3
Bar Magal <barmagal@gmail.com>
2015-04-17 18:29:03 +0300

    Debug coverage failure

--- window-purpose-test.el
+++ window-purpose-test.el
@@ -27,11 +27,15 @@
 
 (message "setting undercover")
 (require 'undercover)
-(undercover "window-purpose.el" "window-purpose-configuration.el"
-            "window-purpose-core.el" "window-purpose-layout.el"
-            "window-purpose-prefix-overload.el" "window-purpose-switch.el"
-            "window-purpose-utils.el"
-            ;; "window-purpose-fixes.el" "window-purpose-x.el"
+(undercover "window-purpose.el"
+            ;; "window-purpose-configuration.el"
+            ;; "window-purpose-core.el"
+            ;; "window-purpose-layout.el"
+            ;; "window-purpose-prefix-overload.el"
+            ;; "window-purpose-switch.el"
+            ;; "window-purpose-utils.el"
+            ;; "window-purpose-fixes.el"
+            ;; "window-purpose-x.el"
             )
 
 (message "loading purpose")
