aed0bf57af9ae0fc4aad5d1e369f1010a2e0b108
bmag <bmagamb@gmail.com>
2016-10-07 09:53:36 +0300

    More switch tests

--- tests/test-switch.el
+++ tests/test-switch.el
@@ -205,6 +205,84 @@
 ;; can't test, because "emacs -batch" can't raise frames
 (describe "switch-to-buffer-other-frame")
 
+(describe "display-buffer"
+  :var (config-snapshot)
+  (before-all
+    (purpose-mode)
+    (setq config-snapshot (get-purpose-config))
+    (load-purpose-config
+     (make-purpose-config :regexps '(("^xxx-p0-" . p0)
+                                     ("^xxx-p1-" . p1)))))
+  (after-all
+    (load-purpose-config config-snapshot)
+    (purpose-mode -1))
+  (before-each
+    (create-buffers "xxx-p0-0" "xxx-p0-1" "xxx-p1-0"))
+
+  (it "doesn't display a buffer when `display-buffer-no-window' is used"
+    (build-one-window '(:name "xxx-p0-0"))
+    (display-buffer "xxx-p0-1" '(display-buffer-no-window (allow-no-window . t)))
+    (expect '(:name "xxx-p0-0") :to-match-window-tree)
+    (display-buffer "xxx-p0-1" '((display-buffer-no-window purpose-display-maybe-other-window)
+                                 (allow-no-window . t)))
+    (expect '(:name "xxx-p0-0") :to-match-window-tree))
+
+  (it "pops a window when `purpose-display-fallback' is `pop-up-window'"
+    (build-one-window '(:name "xxx-p0-0" :b-ded t))
+    (let ((purpose-display-fallback 'pop-up-window))
+      (display-buffer "xxx-p1-0" '(display-buffer-same-window)))
+    (expect '(split (:name "xxx-p0-0" :b-ded t)
+                    (:name "xxx-p1-0" :b-ded nil))
+            :to-match-window-tree))
+  ;; can't test, because "emacs -batch" can't raise frames
+  (it  "pops a frame when `purpose-display-fallback' is `pop-up-frame'")
+  (it "throws an error when `purpose-display-fallback' is `error'"
+    (build-one-window '(:name "xxx-p0-0" :b-ded t))
+    (let ((purpose-display-fallback 'error))
+      (expect (apply-partially 'switch-to-buffer "xxx-p1-0" nil 'force)
+              :to-throw)))
+  (it "falls back to stock `display-buffer' when `purpose-display-fallback' is nil"
+    (build-one-window '(:name "xxx-p0-0" :b-ded t))
+    (let ((purpose-display-fallback nil))
+      (expect (apply-partially 'switch-to-buffer "xxx-p1-0" nil 'force)
+              :to-throw))
+    (build-two-windows '((:name "xxx-p0-0" :p-ded t) (:name "xxx-p0-1" :p-ded t)))
+    (let ((purpose-display-fallback nil))
+      (display-buffer "xxx-p1-0"))
+    (expect '(split (:name "xxx-p0-0" :p-ded t :selected t)
+                    (:name "xxx-p1-0" :p-ded t))
+            :to-match-window-tree)))
+
+(describe "advised switch functions"
+  (before-all
+    (purpose-mode)
+    (spy-on #'purpose-switch-buffer)
+    (spy-on #'purpose-switch-buffer-other-window)
+    (spy-on #'purpose-switch-buffer-other-frame)
+    (spy-on #'purpose-pop-buffer)
+    (spy-on #'purpose-pop-buffer-same-window))
+  (after-all
+    (purpose-mode -1))
+  (it "switch-to-buffer calls purpose-switch-buffer"
+    (insert-user-input "foo")
+    (call-interactively 'switch-to-buffer)
+    (expect 'purpose-switch-buffer :to-have-been-called))
+  (it "switch-to-buffer-other-window calls purpose-switch-buffer-other-window"
+    (insert-user-input "foo")
+    (call-interactively 'switch-to-buffer-other-window)
+    (expect 'purpose-switch-buffer-other-window :to-have-been-called))
+  (it "switch-to-buffer-other-frame calls purpose-switch-buffer-other-frame"
+    (insert-user-input "foo")
+    (call-interactively 'switch-to-buffer-other-frame)
+    (expect 'purpose-switch-buffer-other-frame :to-have-been-called))
+  (it "pop-to-buffer calls purpose-pop-buffer"
+    (insert-user-input "foo")
+    (call-interactively 'pop-to-buffer)
+    (expect 'purpose-pop-buffer :to-have-been-called))
+  (it "pop-to-buffer-same-window calls purpose-pop-buffer-same-window"
+    (pop-to-buffer-same-window "foo")
+    (expect 'purpose-pop-buffer-same-window :to-have-been-called)))
+
 (describe "switch helpers"
   :var (buf1 buf2 buf3)
   (before-each
@@ -218,13 +296,6 @@
     (it "returns non-nil only when window contains buffer"
       (expect (purpose-window-buffer-reusable-p nil buf1) :to-be-truthy)
       (expect (purpose-window-buffer-reusable-p nil buf2) :not :to-be-truthy)))
-
-  ;; (ert-deftest purpose-test-display-no-buffer ()
-  ;; (ert-deftest purpose-test-display-fallback-pop-window ()
-  ;; can't test, because "emacs -batch" can't raise frames
-  ;; (ert-deftest purpose-test-display-fallback-pop-frame ()
-  ;; (ert-deftest purpose-test-display-fallback-error ()
-  ;; (ert-deftest purpose-test-display-fallback-nil ()
 
   (describe "purpose--normalize-width"
     (it "normalizes integers"
@@ -246,10 +317,10 @@
     (it "throws error for negative values")
     (expect (apply-partially 'purpose--normalize-height -5) :to-throw))
 
+  ;; TODO: add equivalents to these old tests
   ;; (ert-deftest purpose-test-display-at ()
   ;; (ert-deftest purpose-test-special-action-sequences ()
   ;; (ert-deftest purpose-cover-select-buffer-without-action-order ()
-  ;; (ert-deftest purpose-test-interactive-switch-buffer ()
   ;; (ert-deftest purpose-test-interactive-switch-buffer-with-some-purpose ()
   ;; (ert-deftest purpose-test-temp-actions-1 ()
   ;; (ert-deftest purpose-test-additional-actions-1 ()
