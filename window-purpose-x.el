a393b85d5a31f2a4a5fa5a720d412d910b4f5c59
bmag <bmagamb@gmail.com>
2018-05-31 20:16:28 +0300

    code1: better display of dired buffer [fix 127]

--- window-purpose-x.el
+++ window-purpose-x.el
@@ -128,8 +128,9 @@ If current buffer doesn't have a filename, do nothing."
   (when (and (buffer-file-name)
              (cl-delete-if #'window-dedicated-p
                            (purpose-windows-with-purpose 'dired)))
+    ;; calling `save-selected-window' is likely unnecessary
     (save-selected-window
-      (dired (file-name-directory (buffer-file-name)))
+      (display-buffer (dired-noselect (file-name-directory (buffer-file-name))))
       (when (fboundp 'dired-hide-details-mode)
         (dired-hide-details-mode))
       (bury-buffer (current-buffer)))))
